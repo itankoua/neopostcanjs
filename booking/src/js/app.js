97a924fc0d8d9b5129e3c61d6ef8283f3fdca88a
Ivan TANKOUA <itankoua@software-phone.com>
2015-10-19 13:35:17 +0200

    Formation 19/10 sources

--- booking/src/js/app.js
+++ booking/src/js/app.js
@@ -1,3 +1,18 @@
 /**
  * Created by itankoua on 16/10/15.
  */
+"use strict";
+
+can.Component.extend({
+    tag: "myTag",
+    template: '<h1 can-click="updateSubject">  {{ subject }} World <content></content> </h1>',
+    scope : {
+        subject : "Hello",
+        updateSubject : function() {
+            this.attr("subject", "Bye bye");
+        }
+    }
+});
+
+
+$("#out").html(can.view("app", {}));
